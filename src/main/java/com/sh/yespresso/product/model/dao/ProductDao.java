557713ff2f81b50e8119c92d7ae2252023e4ca86
khHJP <kh00phj@gmail.com>
2023-01-29 21:49:10 +0900

    ProductService, ProductDao, product-query.properties 수정
    제품 카테고리별 조회 메소드 생성

--- src/main/java/com/sh/yespresso/product/model/dao/ProductDao.java
+++ src/main/java/com/sh/yespresso/product/model/dao/ProductDao.java
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import com.sh.yespresso.product.model.dto.Aroma;
@@ -34,7 +35,7 @@ public class ProductDao {
 	
 
 	/**
-	 * hj start
+	 * **********************   hj start   **********************
 	 */
 	private Product handleProductResultSet(ResultSet rset) throws SQLException{
 		Product product = new Product();
@@ -55,33 +56,125 @@ public class ProductDao {
 		return product;
 	}
 	
-	public List<Product> selectAllProduct(Connection conn, String categoryID) {
-		String sql = prop.getProperty("selectAllProduct"); 
-		List<Product> productList = new ArrayList<>();
-		
-		// 1. PreparedStatement 객체 생성 & 미완성쿼리 값대입
-		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
-			pstmt.setString(1, categoryID); //select * from PRODUCT where FK_CATEGORY_ID = ?
-				
-			// 2. 실행 & ResultSet 반환
-			try(ResultSet rset = pstmt.executeQuery()){
-				
-				// 3. ResultSet -> List<Product>
-				while(rset.next()) {
+	/* DQL 페이지에 해당하는 카테고리별 제품 리스트 불러오기 */
+	// 1. 커피 제품 리스트
+	public List<Product> selectCoffeeList(Connection conn, Map<String, Object> param) {
+		String sql = prop.getProperty("selectCoffeeList");
+		List<Product> coffeeList = new ArrayList<>();
+
+		int page = (int) param.get("page");
+		int limit = (int) param.get("limit");
+
+		int start = (page - 1) * limit + 1;
+		int end = page * limit;
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			try (ResultSet rset = pstmt.executeQuery()) {
+				while (rset.next()) {
 					Product product = handleProductResultSet(rset);
-					productList.add(product);
+					coffeeList.add(product);
 				}
-					
 			}
-			
 		} catch (SQLException e) {
 			throw new ProductException("제품 목록 조회 오류!", e);
 		}
-		return productList;
+		return coffeeList;
 	}
+
+	// 2. 머신 제품 리스트
+	public List<Product> selectMachineList(Connection conn, Map<String, Object> param) {
+		String sql = prop.getProperty("selectMachineList");
+		List<Product> machineList = new ArrayList<>();
+
+		int page = (int) param.get("page");
+		int limit = (int) param.get("limit");
+
+		int start = (page - 1) * limit + 1;
+		int end = page * limit;
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			try (ResultSet rset = pstmt.executeQuery()) {
+				while (rset.next()) {
+					Product product = handleProductResultSet(rset);
+					machineList.add(product);
+				}
+			}
+		} catch (SQLException e) {
+			throw new ProductException("제품 목록 조회 오류!", e);
+		}
+		return machineList;
+	}
+
+	// 3. 악세사리 제품 리스트 
+	public List<Product> selectAccList(Connection conn, Map<String, Object> param) {
+		String sql = prop.getProperty("selectAccList");
+		List<Product> accList = new ArrayList<>();
+
+		int page = (int) param.get("page");
+		int limit = (int) param.get("limit");
+
+		int start = (page - 1) * limit + 1;
+		int end = page * limit;
+
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, start);
+			pstmt.setInt(2, end);
+
+			try (ResultSet rset = pstmt.executeQuery()) {
+				while (rset.next()) {
+					Product product = handleProductResultSet(rset);
+					accList.add(product);
+				}
+			}
+		} catch (SQLException e) {
+			throw new ProductException("제품 목록 조회 오류!", e);
+		}
+		return accList;
+	}
+
+	
+	// DQL categoryId로 카테고리별 제품수 구하기
+	public int getTotalCntById(Connection conn, String categoryId) {
+		String sql = prop.getProperty("getTotalCntById");
+		int totalCount = 0;
+		String msg = "";
+		
+		if("CO".equals(categoryId)) {
+			msg = "커피 제품 수 조회 오류!";
+		} 
+		else if ("MA".equals(categoryId)) {
+			msg = "머신 제품 수 조회 오류!";
+		}
+		else {
+			msg = "악세사리 제품 수 조회 오류!";
+		}
+		
+		try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+			 pstmt.setString(1, categoryId);
+			 
+			 try(ResultSet rset = pstmt.executeQuery()){
+				 if(rset.next()) {
+					totalCount = rset.getInt(1);
+					}
+			 	}
+			} catch (Exception e) {
+				throw new ProductException(msg, e);
+			}
+			return totalCount;
+		}
+
+
+
+
 	
 	/**
-	 * hj end
+	 * *********************   hj end   **********************
 	 */
 
 
@@ -108,4 +201,5 @@ public class ProductDao {
 	 * jooh end
 	 */
 
+	
 }
